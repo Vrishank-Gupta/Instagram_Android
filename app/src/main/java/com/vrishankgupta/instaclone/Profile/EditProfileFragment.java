2932b07b06bf6969c11344c29a2c3670c672a6e8
vrishank <vrishank98@gmail.com>
2018-01-31 22:10:19 +0530

    EditProfileImages

--- app/src/main/java/com/vrishankgupta/instaclone/Profile/EditProfileFragment.java
+++ app/src/main/java/com/vrishankgupta/instaclone/Profile/EditProfileFragment.java
@@ -3,11 +3,15 @@ package com.vrishankgupta.instaclone.Profile;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.vrishankgupta.instaclone.R;
+import com.vrishankgupta.instaclone.Utils.UniversalImageLoader;
 
 /**
  * Created by vrishankgupta on 30/01/18.
@@ -15,10 +19,28 @@ import com.vrishankgupta.instaclone.R;
 
 public class EditProfileFragment extends Fragment{
     private static final String TAG = "EditProfileFragment";
+    private ImageView profilePhoto;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_editprofile,container,false);
+        profilePhoto = view.findViewById(R.id.profile_photo);
+        initImageLoader();
+        setProfileImage();
         return view;
+
+    }
+
+    private void initImageLoader()
+    {
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+
+    private void setProfileImage()
+    {
+        Log.d(TAG, "setProfileImage: ");
+        String imURL = "http://techbeat.com/wp-content/uploads/2013/07/Android-Security-Bug-Found-Hackers-Gain-System-Access.png";
+        UniversalImageLoader.setImage(imURL,profilePhoto,null,"");
     }
 }
---
+++ app/src/main/java/com/vrishankgupta/instaclone/Utils/UniversalImageLoader.java
@@ -0,0 +1,99 @@
+package com.vrishankgupta.instaclone.Utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+import com.vrishankgupta.instaclone.R;
+
+/**
+ * Created by vrishankgupta on 31/01/18.
+ */
+
+public class UniversalImageLoader {
+
+    private  static  final  int defaultImage = R.drawable.ic_android;
+    private Context context;
+
+    public UniversalImageLoader(Context context) {
+        this.context = context;
+    }
+
+    public ImageLoaderConfiguration getConfig()
+    {
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .showImageOnLoading(defaultImage)
+                .showImageForEmptyUri(defaultImage)
+                .showImageOnFail(defaultImage)
+                .cacheOnDisk(true)
+                .cacheInMemory(true)
+                .cacheOnDisk(true).resetViewBeforeLoading(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .displayer(new FadeInBitmapDisplayer(300)).build();
+        ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(context)
+                .memoryCache(new WeakMemoryCache())
+                .diskCacheSize(100 * 1024 *1024).build();
+        return configuration;
+
+    }
+
+    /**
+     * for static images only
+     * @param imgURL
+     * @param image
+     * @param progressBar
+     * @param append
+     */
+
+    public static void setImage(String imgURL, ImageView image, final ProgressBar progressBar,String append)
+    {
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.displayImage(append + imgURL, image, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                if(progressBar != null)
+                {
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+
+                if(progressBar != null)
+                {
+                    progressBar.setVisibility(View.GONE);
+                }
+
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+
+                if(progressBar != null)
+                {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+
+                if(progressBar != null)
+                {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+}
